f46485bf56ad2d4b5b082fa2e7419ea8a78b3600
akniet18 <akniet.akniet2016@mail.ru>
2020-11-15 12:16:12 +0600

    first

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.register(Text)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'chapter')
 admin.site.register(Category)
 admin.site.register(Book)
 admin.site.register(Chapter)
-admin.site.register(Text)
 admin.site.register(Track)
 admin.site.register(TextOptions)
 
